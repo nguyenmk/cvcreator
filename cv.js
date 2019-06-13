2c037caac8a76021fe3151a41c5b210cd4ec5d79
Khoa Nguyen <jckhoa@yahoo.com>
2019-06-13 16:59:07 +0200

    add function to get the region (left, top, right, bottom) of an element the mouse is pointing at

--- cv.js
+++ cv.js
@@ -1,10 +1,35 @@
-
+const Region ={
+	LEFT: "left", RIGHT: "right", TOP: "top", BOTTOM: "bottom"
+}
+function getRegionPointer(box, x, y) {
+	if (box.width > box.height) {
+		wlo = box.width / 6;
+		whi = wlo * 5;
+		hmid = box.height / 2;
+		console.log("region: ", "(" + box.width + "," + box.height + ")" + wlo + " " + whi + " " + hmid);
+		if (x < box.left + wlo) return Region.LEFT;
+		else if (x > box.left + whi) return Region.RIGHT;
+		else if (y < box.top + hmid) return Region.TOP;
+		else return Region.BOTTOM;
+	} else {
+		hlo = box.height / 6;
+		hhi = hlo * 5;
+		wmid = box.width / 2;
+		console.log("region: ", "(" + box.width + "," + box.height + ")" + hlo + " " + hhi + " " + wmid);
+		if (y < box.top + hlo) return Region.TOP;
+		else if (y > box.top + hhi) return Region.BOTTOM;
+		else if (x < box.left + wmid) return Region.LEFT;
+		else return Region.RIGHT;
+	}
+	
+}
 //only exchange elements having the same parent
-function exchangeElements(element1, element2) {
+function exchangeElements(element1, element2, region) {
 	
 	if (element1 == null || element2 == null) return element1;
 	if (element1 == element2) return element1;
 	
+	console.log('Region', region);
 	if (element1.parentNode == element2.parentNode && element1.parentNode != null && element2.parentNode != null) {
 		var clonedElement1 = element1.cloneNode(true);
 		var clonedElement2 = element2.cloneNode(true);
@@ -37,7 +62,8 @@ function dragElement(elmnt) {
 		
     // get element at the mouse cursor
     var elm = document.elementFromPoint(e.clientX, e.clientY);		
-		elmnt = exchangeElements(elmnt, elm);	
+		var r = getRegionPointer(elm.getBoundingClientRect(), e.clientX, e.clientY);
+		elmnt = exchangeElements(elmnt, elm, r);	
   }
 
   function closeDragElement() {
